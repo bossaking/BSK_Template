ebfd1f9de614c88fbc27494bdac45a62d31acf9d
Szymon Woronowicz <szymonworonowicz25@gmail.com>
2021-03-16 23:53:28 +0100

    first commit

---
+++ Krypto/KryptographyService.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Krypto
+{
+    public class KryptographyService
+    {
+        public string DecodeRail(string input, int key)
+        {
+            return null;
+        } 
+        public string EncodeRail(string input, int key)
+        {
+            return null;
+        }         
+        public string EncodeMatrixShift(string input)
+        {
+            return null;
+        }         
+        public string DecodeMatrixShift(string input)
+        {
+            return null;
+        }         
+        public string Encode2b(string input, string key)
+        {
+            return null;
+        } 
+        public string Decode2b(string input, string key)
+        {
+            return null;
+        } 
+        public string Encode2c(string input, string key)
+        {
+            return null;
+        } 
+        public string Decode2c(string input, string key)
+        {
+            return null;
+        } 
+
+        public string EncodeCeaser(string input, int a,int b)
+        {
+            return null;
+        }
+
+        public string DecodeCeaser(string input, int a, int b)
+        {
+            return null;
+        }
+
+        public string EncodeVigenere(string input, string key)
+        {
+            return null;
+        }
+
+        public string DecodeVigenere(string input, string key)
+        {
+            return null;
+        }
+    }
+}
